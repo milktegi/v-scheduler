a940bbd39ba3b69b701a2a89c47f60a33fb94dda
milktegi <milkteegi@gmail.com>
2019-01-30 16:11:11 +0900

    hooked up reducer for creating a todo

--- v-scheduler/src/index.js
+++ v-scheduler/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer);
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
--- v-scheduler/src/store/reducers/projectReducer.js
+++ v-scheduler/src/store/reducers/projectReducer.js
@@ -1,3 +1,5 @@
+import * as types from '../actions/index';
+
 const initialState = {
   projects: [
     { id: '1', title: 'help me find peach', content: 'blah' },
@@ -7,6 +9,11 @@ const initialState = {
 };
 
 const projectReducer = (state = initialState, action) => {
+ 
+	switch(action.type){
+		case types.CREATE_PROJECT:
+		console.log('created project', action.project)
+	}
   return state;
 };
 
