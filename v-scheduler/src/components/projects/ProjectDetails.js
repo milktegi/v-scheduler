d37155b028488b46afdea273d13555e947389170
milktegi <milkteegi@gmail.com>
2019-01-30 14:16:57 +0900

    added navigation to project details page

---
+++ v-scheduler/src/components/projects/ProjectDetails.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ProjectDetails = () => {
+	return (
+		<div className="container section project-details">
+			<div className="card z-depth-0">
+				<div className="card-content">
+					<span className="card-title">
+						일정 이름
+					</span>
+					<p>우연일까 하얀 얼굴 어딘가</p>
+				</div>
+				<div className="card-action grey-lighten-4 grey-text">
+					<div>milktegi님이 등록함</div>
+					<div>01.30 6:00 </div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ProjectDetails;
+
