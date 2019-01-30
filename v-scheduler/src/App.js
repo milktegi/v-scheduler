28b8f2f9dc6d2baec904b6678457bf4a59a9f363
milktegi <milkteegi@gmail.com>
2019-01-30 14:41:23 +0900

    added signIn & up form

--- v-scheduler/src/App.js
+++ v-scheduler/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar'
+import Navbar from './components/layout/Navbar';
 import DashBoard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-         <Navbar/>
-         <Switch>
-           <Route path="/" exact component={DashBoard}/>
-           <Route path="/project/:id" component={ProjectDetails}/>
-         </Switch>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={DashBoard} />
+            <Route path="/signin" exact component={SignIn} />
+            <Route path="/signup" exact component={SignUp} />
+            <Route path="/project/:id" component={ProjectDetails} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
