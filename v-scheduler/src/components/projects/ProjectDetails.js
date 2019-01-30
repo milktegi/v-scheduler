84019cbc3f727ed34f80d4f73241da34530ef938
milktegi <milkteegi@gmail.com>
2019-01-30 19:31:33 +0900

    rendered individual post

--- v-scheduler/src/components/projects/ProjectDetails.js
+++ v-scheduler/src/components/projects/ProjectDetails.js
@@ -4,45 +4,50 @@ import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 
-const ProjectDetails = (props) => {
-
-	const id = props.match.params.id;
-
-	return (
-		<div className="container section project-details">
-			<div className="card z-depth-0">
-				<div className="card-content">
-					<span className="card-title">
-						일정 이름
-					</span>
-					<p>우연일까 하얀 얼굴 어딘가 {id}</p>
-				</div>
-				<div className="card-action grey-lighten-4 grey-text">
-					<div>milktegi님이 등록함</div>
-					<div>01.30 6:00 </div>
-				</div>
-			</div>
-		</div>
-	)
-}
+const ProjectDetails = props => {
+  const id = props.match.params.id;
+  console.log(props);
+  const { project } = props;
+  if (project) {
+    return (
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
+          </div>
+          <div className="card-action grey-lighten-4 grey-text">
+            <div>milktegi님이 {project.author}</div>
+            <div>01.30 6:00 </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <p>loading...</p>
+      </div>
+    );
+  }
+};
 
 const mapStateToProps = (state, ownProps) => {
-	console.log(state);
-	const id = ownProps.match.params.id;
-	const projects = state.firestore.data.projects;
-
-	return {
-		// individual project!
-
-	}
-}
+  // console.log(state);
+  const id = ownProps.match.params.id;
+  const projects = state.firestore.data.projects;
+  const project = projects ? projects[id] : null;
+  return {
+    // individual project!
+    project
+  };
+};
 
 export default compose(
- connect(mapStateToProps),
- firestoreConnect([
-	 {
-		 collection: 'projects'
-	 }
- ])
+  connect(mapStateToProps),
+  firestoreConnect([
+    {
+      collection: 'projects'
+    }
+  ])
 )(ProjectDetails);
-
--- v-scheduler/src/store/reducers/projectReducer.js
+++ v-scheduler/src/store/reducers/projectReducer.js
@@ -1,11 +1,7 @@
 import * as types from '../actions/index';
 
 const initialState = {
-  projects: [
-    { id: '1', title: 'help me find peach', content: 'blah' },
-    { id: '2', title: 'help me find lemon', content: 'blah blah' },
-    { id: '3', title: 'help me find lime', content: 'blah blah blah' }
-  ]
+  projects: [{}]
 };
 
 const projectReducer = (state = initialState, action) => {
