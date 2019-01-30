b7c3972f2c179b264a19e59b5ef256f8aa5b7e5d
milktegi <milkteegi@gmail.com>
2019-01-31 03:33:31 +0900

    notification triggers cloud-function

--- v-scheduler/src/components/dashboard/Notification.js
+++ v-scheduler/src/components/dashboard/Notification.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const Notification = () => {
-	return (
-		<div>
-			<p>Notification</p>
-		</div>
-	)
-}
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <span className="card-title">알림</span>
+        <ul className="notification">
+          <li>notify</li>
+          <li>notify</li>
+          <li>notify</li>
+          <li>notify</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Notification;
