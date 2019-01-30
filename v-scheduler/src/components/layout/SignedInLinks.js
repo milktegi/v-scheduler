8ba4e790cb9fe16a87cdb4cfd55a4e6681c8e955
milktegi <milkteegi@gmail.com>
2019-01-30 15:10:35 +0900

    improved some styling

--- v-scheduler/src/components/layout/SignedInLinks.js
+++ v-scheduler/src/components/layout/SignedInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const SignedInLinks = () => {
 	return (
 		<ul className="right">
-			<li><NavLink to="/">새 일정</NavLink></li>
+			<li><NavLink to="/create">새 일정</NavLink></li>
 			<li><NavLink to="/">로그아웃</NavLink></li>
 			<li><NavLink to="/" 
 			className="btn btn-floating pink lighten-1"
